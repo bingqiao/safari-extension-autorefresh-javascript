d90f7426e44208558b48b0deb23f1a918fd35e00
Bing Qiao <bqiaodev@gmail.com>
2021-10-26 20:08:57 +0100

    Initial Commit

---
+++ autorefresh/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  autorefresh
+//
+//  Created by Bing Qiao on 26/10/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
